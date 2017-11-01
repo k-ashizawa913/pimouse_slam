6367e6ecc8b6943c19ef985e2a12d4323297a3e5
Kotaro Ashizawa <k-ashizawa@hykwlab.org>
2017-11-01 07:42:11 +0000

    motor

--- scripts/motors.py
+++ scripts/motors.py
@@ -6,6 +6,7 @@ from geometry_msgs.msg import Twist, Quaternion, TransformStamped, Point
 from std_srvs.srv import Trigger, TriggerResponse
 from pimouse_ros.srv import TimedMotion
 from nav_msgs.msg import Odometry
+from visualization_msgs.msg import Marker
 
 class Motor():
     def __init__(self):
@@ -24,6 +25,7 @@ class Motor():
 	self.using_cmd_vel = False
 
 	self.pub_odom = rospy.Publisher('odom', Odometry, queue_size=10)
+	self.pub_mark = rospy.Publisher('mark' Marker, queue_size=10)
 	self.bc_odom = tf.TransformBroadcaster()
 
 	self.x, self.y, self.th = 0.0, 0.0, 0.0
@@ -109,17 +111,49 @@ class Motor():
 	self.bc_odom.sendTransform((self.x,self.y,0.0), q, self.cur_time,"base_link","odom")
 
 	odom = Odometry()
+
 	odom.header.stamp = self.cur_time
 	odom.header.frame_id = "odom"
-	odom.child_frame_id = "base_link"
 
+        odom.pose.pose.position = Point(self.x,self.y,0)
+        odom.pose.pose.orientation = Quaternion(*q)
+
+	odom.child_frame_id = "base_link"
 	odom.twist.twist.linear.x = self.vx
 	odom.twist.twist.linear.y = 0.0
 	odom.twist.twist.angular.z = self.vth
 
 	self.pub_odom.publish(odom)
 	
-	self.last_time = self.cur_time	
+	self.last_time = self.cur_time
+
+    def send_mark(self):
+        self.cur_time = rospy.Time.now()
+
+        dt = self.cur_time.to_sec() - self.last_time.to_sec()
+        self.x += self.vx * math.cos(self.th) * dt
+        self.y += self.vx * math.sin(self.th) * dt
+        self.th += self.vth * dt
+
+        q = tf.transformations.quaternion_from_euler(0, 0, self.th)
+        self.bc_mark.sendTransform((self.x,self.y,0.0), q, self.cur_time,"base_link","mark")
+
+	mark = Marker()
+
+        mark.header.stamp = self.cur_time
+        mark.header.frame_id = "mark"
+
+        mark.pose.pose.position = Point(self.x,self.y,0)
+        mark.pose.pose.orientation = Quaternion(*q)
+
+        mark.child_frame_id = "base_link"
+        mark.twist.twist.linear.x = self.vx
+        mark.twist.twist.linear.y = 0.0
+        mark.twist.twist.angular.z = self.vth
+
+        self.pub_odom.publish(mark)
+
+        self.last_time = self.cur_time
 
 
 if __name__ == '__main__':
